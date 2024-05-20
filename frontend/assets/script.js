e5d61f0ea79ddbebfd9c3c95918fea3466dd20c8
Erick Valente <erickhwk@gmail.com>
2024-05-20 10:14:01 -0300

    change cors origin to accept all

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -1,9 +1,8 @@
-// function onSubmit(token) {
-//   document.getElementById("votingForm").submit();
-// }
+function onSubmit(token) {
+  document.getElementById("votingForm").submit();
+}
 
 const apiUrl = "http://localhost:3000/api/v1/contests/1"
-const apiUrl2 = "https://jsonplaceholder.typicode.com/todos/1"
 
 fetch(apiUrl)
       .then(response => response.json())
