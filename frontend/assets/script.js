6b948a51d8ee850f79cd476dd8458660ce25aea2
Erick Valente <erickhwk@gmail.com>
2024-05-19 23:25:03 -0300

    setup cors

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -1,6 +1,6 @@
-function onSubmit(token) {
-  document.getElementById("votingForm").submit();
-}
+// function onSubmit(token) {
+//   document.getElementById("votingForm").submit();
+// }
 
 const apiUrl = "http://localhost:3000/api/v1/contests/1"
 const apiUrl2 = "https://jsonplaceholder.typicode.com/todos/1"
