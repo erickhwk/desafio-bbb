0adf19c292ab73413286f456828bb8fba6d7727a
Erick Valente <erickhwk@gmail.com>
2024-05-21 15:44:21 -0300

    add info fetched from api to pannel page

---
+++ frontend/assets/js/pannel.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const voteSummaryApiUrl = 'http://localhost:3000/api/v1/contests/1/votes_summary';
+
+  fetch(voteSummaryApiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      const votesPartialContainer = document.getElementById('participants-summary');
+      votesPartialContainer.innerHTML = '';
+
+      function votesPercentage(participant_votes, total_votes) {
+        const participant_vote_percentage = (participant_votes / total_votes) * 100;
+        return participant_vote_percentage.toFixed(2);
+      }
+
+      Object.keys(data.participants).forEach(participantKey => {
+        const participant = data.participants[participantKey];
+        
+        const participantPartialDiv = document.createElement('div');
+        participantPartialDiv.classList.add('vote-summary');
+
+        participantPartialDiv.innerHTML = `
+            <div class="participant-name">${participant.name}</div>
+            <div class="percentage">${votesPercentage(participant.votes, data.total_votes)}%</div>
+            <div class="total-votes">
+              <span>Total:</span><span>${participant.votes}</span>
+            </div>
+        `;
+
+        votesPartialContainer.appendChild(participantPartialDiv);
+      });
+
+      const totalVotesContainer = document.getElementById('total-summary');
+      totalVotesContainer.innerHTML = '';
+
+      const totalVotesPartialDiv = document.createElement('div');
+      totalVotesPartialDiv.classList.add('vote-summary');
+
+      totalVotesPartialDiv.innerHTML = `
+        <div class="participant-name">Votação Total</div>
+        <div class="percentage">${data.total_votes}</div>
+        <div class="last-hour-votes">
+          <span>Votos na última hora:</span><span>${data.votes_in_last_hour}</span>
+        </div>
+      `;
+
+      totalVotesContainer.appendChild(totalVotesPartialDiv);
+
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
