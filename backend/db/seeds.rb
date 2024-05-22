623cbc5ce267c573ac2d53505ae99f8f5cc3297c
Erick Valente <erickhwk@gmail.com>
2024-05-22 18:11:59 -0300

    improve seed creation

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,26 +1,30 @@
 puts "Generating participants for this season..."
 
 participants = [
-  "Juliette Freire",
-  "Gilberto Nogueira",
-  "Fiuk",
-  "Camilla de Lucas",
-  "Arthur Picoli",
-  "Pocah",
-  "Carla Diaz",
-  "Sarah Andrade",
-  "Rodolffo Matthaus",
-  "Thaís Braz",
-  "Viih Tube",
-  "Caio Afiune",
-  "João Luiz Pedrosa",
-  "Kerline Cardoso",
-  "Lucas Penteado",
-  "Lumena Aleluia",
-  "Nego Di",
-  "Projota",
-  "Karol Conká",
-  "Arcrebiano de Araújo",
+  "Pelé",
+  "Ayrton Senna",
+  "Xuxa",
+  "Gisele Bündchen",
+  "Paulo Coelho",
+  "Roberto Carlos",
+  "Ivete Sangalo",
+  "Neymar",
+  "Anitta",
+  "Ronaldinho Gaúcho",
+  "Silvio Santos",
+  "Fernanda Montenegro",
+  "Chico Buarque",
+  "Caetano Veloso",
+  "Gilberto Gil",
+  "Adriana Lima",
+  "Wesley Safadão",
+  "Sandy",
+  "Zezé Di Camargo",
+  "Luciano Huck",
+  "Marina Silva",
+  "Lázaro Ramos",
+  "Taís Araújo",
+  "Rodrigo Santoro"
 ]
 
 participants.each do |participant_name|
@@ -30,13 +34,18 @@ end
 puts "Participants created successfully!"
 
 puts "Creating the first Contest..."
-first_participant_id = rand(1..20)
-second_participant_id = rand(1..20)
+
+last_participant_index = participants.lenght-1
+
+first_participant_id = rand(1..last_participant_index)
+second_participant_id = rand(1..last_participant_index)
 
 until second_participant_id != first_participant_id
-  second_participant_id = rand(1..20)
+  second_participant_id = rand(1..last_participant_index)
 end
 
 Contest.create(first_participant_id: first_participant_id, 
                second_participant_id: second_participant_id
 )
+
+puts "Contest created successfully!"
